c2e4b8b110b31f5570ab9b2c905a214feb646cbb
atharvadhumal <atharvaadhumal@gmail.com>
2026-05-13 17:50:41 +0530

    learning variables in java

---
+++ dsaJava/variables/Main.java
@@ -0,0 +1,10 @@
+package dsaJava.variables;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 20;
+        String name = "atharva";
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
