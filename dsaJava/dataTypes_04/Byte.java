f26669e90746ee9ce50b5d228203f8fdc7aff91d
atharvadhumal <atharvaadhumal@gmail.com>
2026-05-14 16:26:50 +0530

    learned about data types

---
+++ dsaJava/dataTypes_04/Byte.java
@@ -0,0 +1,14 @@
+package dsaJava.dataTypes_04;
+
+public class Byte {
+    public static void main(String[] args) {
+        byte a = 125;
+        long b = 121217218271827122l;
+        short c = 23461;
+        double d =17.8;
+        float e = 14.5f;
+        boolean flag = true;
+        char ch = 'a';
+
+    }  
+}
