7402bcbe227377c85538099aded6767f2ed41b66
atharvadhumal <atharvaadhumal@gmail.com>
2026-05-14 11:45:57 +0530

    learned about operators, concatenation

---
+++ dsaJava/variables/Question.java
@@ -0,0 +1,9 @@
+package dsaJava.variables;
+
+public class Question {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+            System.out.println("Sum of " + a +" and " + b + " = " + (a+b));
+    }
+}
